60414194bb6415ad42123d5ec146c2cd0d663c63
SakabeTomoya <61626658+yotsutose@users.noreply.github.com>
2022-04-27 20:32:34 +0900

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# cli
 brew install git
 brew install docker
 
+# gui
 brew install --cask docker
 brew install --cask google-chrome
 brew install --cask visual-studio-code
@@ -12,11 +15,11 @@ brew install --cask slack
 brew install --cask discord
 brew install --cask notion
 brew install --cask zoom
-brew install --cask anki
-brew install --cask epoccam
+brew install --cask shiftit
+# brew install --cask anki
+# brew install --cask epoccam
 # brew install --cask karabiner-elements
 
 # brew installや brew upgradeするたびに前のバージョンをキャッシュしてくれるので
 # brew cleanupしたほうがいい
-
 # docker desctopは入らないかもしれない
